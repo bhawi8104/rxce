262ac9c287c25d348a0d2155fe228a544eb04311
bhawi8104 <95489879+bhawi8104@users.noreply.github.com>
2021-12-04 08:30:49 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -60,49 +60,59 @@ def hero():
         # for mac and linux(here, os.name is 'posix')
         else:
             _ = system('clear')
-    def getSum(n):
-        sum=0
-        for digit in str(n):
-            sum+= int(digit)
-        return sum
 
-    def lawde_time_pe_khel(n):
-        check=0
-        for digit in (n):
-            if(int(digit)==0):
-                check=check+1
-        return check
     clear()
     y=1
     newperiod=period
-    banner='figlet RXCE'
+    banner='figlet RXCE V 2.0'
+    thisway=[2,6,8,11,12,15,16,18,19,20]
+    thatway=[1,3,4,5,7,9,10,14,13,17]
     numbers=[]
+    i=1
     while(y):
         clear()
         system(banner)
-        print("Contact me on telegram @smsn_knt")
+        print("Contact me on telegram @tusharma3")
         print("Enter ",newperiod," Parity Price :")
         current=input()
         current=int(current)
         chalo()
-        print("\n---------Successfully hacked the server-----------")
+        print("\n---------Successfully Connected to the server-----------")
         chalo1()
         print("\n---------Successfully got the colour -------------")
         print('\n')
-        last2=str(current)[-2:]
-        samjha_maadarchod=lawde_time_pe_khel(last2)
-        if(newperiod%2==0):
-            sum=getSum(current)
-            if(sum%2==0):
-                print(newperiod+1," : 🔴, RED")
+        def getSum(n):
+            sum=0
+            for digit in str(n):
+                sum += int(digit)
+            return sum
+        if i in thisway:
+            m=getSum(current)
+            n=int(current)%10
+            if((m%2==0 and n%2==0) or (m%2==1 and n%2==1)):
+                if current in numbers:
+                    print(newperiod+1," : Red 🔴")
+                else:
+                    print(newperiod+1," : Green 🟢")
             else:
-                print(newperiod+1,"  : 🟢, GREEN")
-        else:
-            sum=getSum(current)
-            if(sum%2==0):
-                print(newperiod+1,"   : 🔴, RED")
+                if current in numbers:
+                    print(newperiod+1," : Green 🟢")
+                else:
+                    print(newperiod+1," : Red 🔴")
+        if i in thatway:
+            m=getSum(current)+1
+            n=int(current)%10
+            if((m%2==0 and n%2==0) or (m%2==1 and n%2==1)):
+                if current in numbers:
+                    print(newperiod+1,": Green🟢")
+                else:
+                    print(newperiod+1,": Red 🔴")
             else:
-                print(newperiod+1,"   : 🟢, GREEN")
+                if current in numbers:
+                    print(newperiod+1,": Red 🔴")
+                else:
+                    print(newperiod+1,": Green 🟢")
+        i=i+1
         newperiod+=1
         numbers.append(current)
         y=input("Do you want to play : Press 1 and 0 to exit \n")
@@ -113,121 +123,49 @@ def hero():
             system('figlet Thank you!!')
             print("Play on next specified time!!")
             print("-----------Current Time UP----------")
-            sys.exit(" \n \n \n Contact on Telegram @smsn_knt")
-            #print(numbers)
+            sys.exit(" \n \n \n Contact on Telegram @tusharma3")
+            print(numbers)
   
 
 
 
 if(expirydate>today):
     now = datetime.datetime.now()
-    First = now.replace(hour=13, minute=55, second=0, microsecond=0)
-    Firstend = now.replace(hour=14, minute=35, second=0, microsecond=0)
-    Second = now.replace(hour=16, minute=25, second=0, microsecond=0)
-    Secondend = now.replace(hour=17, minute=35, second=0, microsecond=0)
-    Third = now.replace(hour=15, minute=55, second=0, microsecond=0)
-    Thirdend = now.replace(hour=16, minute=35, second=0, microsecond=0)
-    Final = now.replace(hour=17, minute=55, second=0, microsecond=0)
-    Finalend = now.replace(hour=18, minute=35, second=0, microsecond=0)
+    First = now.replace(hour=10, minute=55, second=0, microsecond=0)
+    Firstend = now.replace(hour=11, minute=35, second=0, microsecond=0)
+    Second = now.replace(hour=13, minute=55, second=0, microsecond=0)
+    Secondend = now.replace(hour=14, minute=35, second=0, microsecond=0)
+    Third = now.replace(hour=17, minute=55, second=0, microsecond=0)
+    Thirdend = now.replace(hour=18, minute=35, second=0, microsecond=0)
+    Final = now.replace(hour=19, minute=55, second=0, microsecond=0)
+    Finalend = now.replace(hour=20, minute=35, second=0, microsecond=0)
 
-    if (now>Third and now<Thirdend):
-            period=320
+    if (now>First and now<Firstend):
+            period=220
             hero()
-    elif(False):
-            period=340
+    elif(now>Second and now<Secondend):
+            period=280
             hero()
-    elif(False):
-            period=340
-            hero()
-    elif(False):
+    elif(now>Third and now<Thirdend):
             period=360
             hero()
+    elif(now>Final and now<Finalend):
+            period=400
+            hero()
+    #elif(True):
+    #        period=20211010304
+    #        hero()
     else:
-        banner='figlet RXCE'
-        system(banner)
-        #print("Hi!! Thanks for buying the hack")
+        banner='figlet RXCE V 2.0'
+        print("Hi!! Thanks for buying the hack")
         print("Hi! thanks for trying our DEMO")
         print("----------Your play time-----------")
-        #print("31st Aug 2021, 11:00 AM- 11:30 AM")
-        #print("31st Aug 2021, 02:00 PM- 02:30 PM")
-        print("23rd Sept 2021, 04:00 PM- 04:30 PM")
-        #print("31st Aug 2021, 08:00 PM- 08:30 PM")
+        print(" sept 2021, 11:00 AM- 11:30 AM")
+        print(" sept 2021, 02:00 PM- 02:30 PM")
+        print(" Sept 2021, 06:00 PM- 06:30 PM")
+        print(" sept 2021, 08:00 PM- 08:30 PM")
         print("Please play on the given time, and ")
         print("If you think it is an error contact")
-        print(" admin on telegram @smsn_knt ")
-
-
-
-else:
-    def clear():
-        # for windows
-        if name == 'nt':
-            _ = system('cls')
-        # for mac and linux(here, os.name is 'posix')
-        else:
-            _ = system('clear')
-    code="MRP1"
-    test="chumma4"
-    #nextday="NITESH9013"
-    banner='figlet RXCE'
-    system(banner)
-    print("*---------*----------*-------------*----------*")
-    print("Your hack has expired--- Please contact")
-    print(" on telegram ----@smsn_knt for activating")
-    print(" Recharge Amount :        Total limit " )
-    print(" 1.     1000 INR -------  1 Day (30 Games")
-    print(" 2.     5000 INR -------  7 Days(210 Games")
-    print("*---------*----------*-------------*----------*")
-    print("Your custom hack can be made request from us.")
-    print("Beware of fraudsters!!!")
-    while(True):
-        print("My banking name is SUNNY KUMAR")
-        print("If you send to any other name , then IT IS SCAMMM")
-        print("--------*--------*----------*---------")
-        print("send payment only to SUNNY KUMAR ")
-        print("payhere--- UPI : ")
-        print("UPI1 : mdurth@ybl")
-        print("UPI2 : sunnyk16@fbl")
-        print("If you have already paid to above UPI")
-        print("Please Enter your 12 Digit \n UPI reference number ")
-        print("or please Enter Correct activation code for 8:30 PM ")
-        bhai=input(": ")
-        if(bhai==code or bhai==test):
-            clear()
-            print("----------Your play time-----------")
-            print("3rd Dec 2021, 02:30 PM- 03:00 PM")
-            print("3rd Dec 2021, 05:30 PM- 06:00 PM")
-            print("3rd Dec 2021, 08:30 PM- 09:00 PM")
-            print("Please play on the given time, and ")
-            print("If you think it is an error contact")
-            print("wait.... starting....")
-            time.sleep(20)
-            period=162
-            hero()
-            #print("Today Server is off because I am out ")
-            #rint(" of town, Tomorrow It will work as usual.")
-            #print(" Thank You!!")
-            #rint("To all the weekly members next week, cost will be  ")
-            #print(" 4000 INR , because in this week 2 days off " )
-            #print("Thank You!! ")
-            sys.exit(" \n \n \n Contact on Telegram @smsn_knt")
-        elif(bhai==nextday or bhai==nexday1):
-            clear()
-            banner='figlet RXCE'
-            system(banner)
-            print("----------Your play time-----------")
-            #print("8th-14th Nov 2021, 02:30 PM- 03:00 PM")
-            #print("8th-14th Nov 2021, 06:00 PM- 06:30 PM")
-            #print("8th-14th Nov 2021, 08:30 PM- 09:00 PM")
-            #print("Please play on the given time, and ")
-            #print("If you think it is an error contact")
-            #print("wait.... starting....")
-            time.sleep(20)
-            period=420
-            hero()
-            period("Sorry too many people(>20) using hack in same time ")
-            sys.exit(" \n \n \n Contact on Telegram @smsn_knt")
-            
             
         else:
             clear()
